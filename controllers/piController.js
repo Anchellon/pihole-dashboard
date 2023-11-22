638e63b00c09c9a13ca76dc85fbb7f7ab67e91f4
anchellon <anshulmathew@gmail.com>
2023-11-21 21:33:46 -0800

    Comments added

--- controllers/piController.js
+++ controllers/piController.js
@@ -1,7 +1,9 @@
+// js Sqlite3 Driver
 var sqlite3 = require("sqlite3").verbose();
 var fs = require("fs");
 var dbFile = "/etc/pihole/gravity.db"; //file locations of the sqlite3 db
 var dbExists = fs.existsSync(dbFile);
+// Checking if DB Exists
 if (!dbExists) {
     console.log("DB Doesn't Exist");
 }
@@ -17,6 +19,10 @@ var db = new sqlite3.Database(dbFile);
 // INSERT INTO domainlist VALUES
 //(3,<linktext>,true, Date.now() / 1000,Date.now() / 1000,"")
 
+// Function to generate Query Strings
+// the link submitted are set to regex ie value 3
+// The date added and modified are the subsequent fields
+// No comments are added by default can improve on it iff required
 function generateQueryString(links) {
     // let links = ["reddit.com", "facebook.com"];
     let str = "";
@@ -37,9 +43,11 @@ function generateQueryString(links) {
     return str;
 }
 
+// This method taks in a request with the links
+// It is then added into the corresponding gravity db of the pihole so that functioning can take place
 exports.link_create_postMethod = async (req, res) => {
-    // let links = req.body.links;
-    let links = ["reddit.com", "facebook.com"];
+    let links = req.body.links;
+    // let links = ["reddit.com", "facebook.com"];
     let queryString =
         "INSERT INTO domainlist VALUES" + generateQueryString(links);
 
@@ -54,3 +62,4 @@ exports.link_create_postMethod = async (req, res) => {
     db.close();
     res.send(200, "Success");
 };
+// Further work ability to enable and disable links, Potentially submit cron jobs too
