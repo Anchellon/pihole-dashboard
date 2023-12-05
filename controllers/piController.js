9244e69c21b6fd04b90f38531a4214922b1e2c55
anchellon <anshulmathew@gmail.com>
2023-12-05 02:52:03 -0800

    fixing sql

--- controllers/piController.js
+++ controllers/piController.js
@@ -85,7 +85,7 @@ exports.link_create_postMethod = async (req, res) => {
         }
         rows.forEach((row) => {
             lastId = row.id;
-            console.log(lastId + "hi");
+            console.log("bruh" + lastId + "hi");
         });
     });
     console.log(lastId);
